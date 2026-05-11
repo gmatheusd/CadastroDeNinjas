607366b841a953bae14d62b85995d91fdff6eee7
unknown <g.matheus.d@outlook.com>
2026-05-11 14:35:01 -0300

    Criado a controller.java [Aula 2]

---
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Controller.java
@@ -0,0 +1,13 @@
+package br.com.monkeyscript.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
