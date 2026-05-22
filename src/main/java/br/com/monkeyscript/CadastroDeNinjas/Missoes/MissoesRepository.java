d5108bd8a477a5e5f5e127f4580398f223649148
unknown <g.matheus.d@outlook.com>
2026-05-22 15:15:38 -0300

    feat: MissoesRepository e NinjaRepository criados e configorados com o ORM JpaRepository

---
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,7 @@
+package br.com.monkeyscript.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+
+}
---
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,7 @@
+package br.com.monkeyscript.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
+}
