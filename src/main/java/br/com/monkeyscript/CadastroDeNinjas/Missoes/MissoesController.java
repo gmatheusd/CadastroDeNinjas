ddbd437ef21648da1185e2bf1c66c9d3b2a0cb1d
unknown <g.matheus.d@outlook.com>
2026-07-22 11:53:27 -0300

    feat: Cria as rotas do controller da missoes com mock

--- src/main/java/br/com/monkeyscript/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,8 +1,6 @@
 package br.com.monkeyscript.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/missoes")
@@ -11,6 +9,31 @@ public class MissoesController {
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Essa é minha mensagem na rota de missoes";
+    }
+
+
+    // GET -- Mandar uma requisisao para mostrar as missoes
+    @GetMapping("/listar")
+    public String mostrarMissao() {
+        return "Missoes mostradas sucesso";
+    }
+
+    // POST -- Manda uma requisisao para criar uma missao
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    // PUT -- Manda uma requisisao para alterar as missoes
+    @PutMapping("/alterarID")
+    public String alterarMissaoPorId() {
+        return "Missao alterada com sucesso";
+    }
+
+    // Delete -- Mandar uma requisisao para deletar as missoes
+    @DeleteMapping("/deletarID")
+    public String deletarMissaoPorId() {
+        return "Missao deletada com sucesso";
     }
 }
 
