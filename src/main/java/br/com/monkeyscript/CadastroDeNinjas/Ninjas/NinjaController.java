0b1c0e85071350fc8917405fef89410de636bd08
unknown <g.matheus.d@outlook.com>
2026-07-22 11:24:01 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -10,4 +10,36 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
     }
+
+    // Adicionar ninja (CREATE)
+    @GetMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninjas";
+    }
+
+    // Mostrar ninja por Id (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar Ninjas por ID";
+    }
+
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping ("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por ID";
+    }
+
+    // Deletar Ninjas (DELETE)
+    @DeleteMapping ("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por id";
+    }
+
 }
