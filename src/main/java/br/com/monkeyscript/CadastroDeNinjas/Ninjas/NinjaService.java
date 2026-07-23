2b68db5ba1f6921f08f4a9702254d2630887255f
unknown <g.matheus.d@outlook.com>
2026-07-23 12:09:36 -0300

    feat: Inclui a rota de exclusao de ninjas usando o metodo DELETE

--- src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -29,4 +29,9 @@ public class NinjaService {
     public NinjaModel criarNinja(NinjaModel ninja) {
         return ninjaRepository.save(ninja);
     }
+
+    // Deletar um ninja -- tem que ser um metodo VOID
+    public void deletarNinjaPorId(Long id) {
+        ninjaRepository.deleteById(id);
+    }
 }
