2df07f59bd05637e17353bbca9c863ec1bcf83b4
unknown <g.matheus.d@outlook.com>
2026-07-24 16:32:02 -0300

    fix: Iclui a coluna de rank na minha entidade, porem ja estava incluida na minha migration V2

--- src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/br/com/monkeyscript/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -32,6 +32,9 @@ public class NinjaModel {
     @Column (name = "idade")
     private int idade;
 
+    @Column (name = "rank")
+    private String rank;
+
     // @ManyToOne um ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missao_id") // Foreing Key ou Chave estrangeira
